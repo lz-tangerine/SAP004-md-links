ebceb56e42af4aa54809723051727e877701cb70
Carol Moscheta <carolina.moscheta@gmail.com>
2020-07-06 13:56:17 -0300

    função que chama o arquivo

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 module.exports = () => {
-  // ...
+  const fs = require('fs')
+  fs.readdir(__dirname, err, files) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("\nCurrent directory filenames:");
+      files.forEach(file => {
+        console.log(file);
+      })
+    }
+  }
 };
