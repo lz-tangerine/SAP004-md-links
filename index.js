ebb73f2dd911ee9fa7335d1f3b959cfcde8d68f4
Carol Moscheta <carolina.moscheta@gmail.com>
2020-07-06 19:05:53 -0300

    função que le o diretorio e o arquivo md

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 module.exports = () => {
-  const fs = require('fs')
-  fs.readdir(__dirname, err, files) => {
-    if (err)
-      console.log(err);
-    else {
-      console.log("\nCurrent directory filenames:");
-      files.forEach(file => {
-        console.log(file);
-      })
-    }
-  }
+  read();
 };
+
+function read() {
+  const fs = require('fs');
+  fs.readdir(__dirname, (err, files) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("\nCurrent directory filenames:");
+        files.forEach(file => {
+          console.log(file);
+        });
+      }
+    },
+
+    fs.readFile("./README.md", "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(data)
+      }
+    }))
+}
+read()
