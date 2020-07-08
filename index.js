29616046f08e4eabfd0271892a4423074312b86c
Carol Moscheta <carolina.moscheta@gmail.com>
2020-07-08 11:14:42 -0300

    install markdown-it

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
-const {
-  pathToFileURL
-} = require('url');
+const markdownIt = require("markdown-it");
+const fs = require("fs");
+const md = new markdownIt();
+
+// const {
+//   pathToFileURL
+// } = require('url');
 
 module.exports = () => {
-  mdLinks();
-};
-
-const fs = require('fs');
-
-function mdLinks() {
-  const path = (require('path'))
+  const path = require("path");
   fs.readdir(__dirname, (err, files) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Filenames with the .md extension:");
-        files.forEach(file => {
-          if (path.extname(file) == ".md") {
-            console.log(file);
-          }
-        });
-      }
-    },
-
-    fs.readFile("./README.md", "utf8", (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(data)
-      }
-    }))
-}
-mdLinks()
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Filenames with the .md extension:");
+      files.forEach((file) => {
+        if (path.extname(file) == ".md") {
+          console.log(file);
+          fs.readFile(file, "utf8", (err, data) => {
+            if (err) {
+              console.log(err);
+            } else {
+              const result = md.render(data);
+              console.log(result);
+            }
+          });
+        }
+      });
+    }
+  });
+};
