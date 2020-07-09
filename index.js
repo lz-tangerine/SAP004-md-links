4dc5f24e2186b60bb7334f6dea6b51e4479af5e1
Carol Moscheta <carolina.moscheta@gmail.com>
2020-07-09 20:46:07 -0300

    JSDOM para puxar os links

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const markdownIt = require("markdown-it");
 const fs = require("fs");
 const md = new markdownIt();
-
-// const {
-//   pathToFileURL
-// } = require('url');
+const path = require("path");
+const {
+  JSDOM
+} = require("jsdom");
 
 module.exports = () => {
-  const path = require("path");
   fs.readdir(__dirname, (err, files) => {
     if (err) {
       console.log(err);
     } else {
       console.log("Filenames with the .md extension:");
       files.forEach((file) => {
-        if (path.extname(file) == ".md") {
+        if (path.extname(file) === ".md") {
           console.log(file);
           fs.readFile(file, "utf8", (err, data) => {
             if (err) {
               console.log(err);
             } else {
               const result = md.render(data);
-              console.log(result);
+              const dom = new JSDOM(`<!DOCTYPE html><p>${result}</p>`);
+              const getLink = dom.window.document.querySelectorAll("a");
+              for (let a of getLink) {
+                console.log(a.href);
+              }
             }
           });
         }
---
+++ test.js
@@ -0,0 +1,2 @@
+let x = require('./index.js')
+console.log(x())
