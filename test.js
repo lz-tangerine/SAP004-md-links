f96e0f21e5700a30fc1e63f8c21bd71256e19671
Carol Moscheta <carolina.moscheta@gmail.com>
2020-07-13 11:06:22 -0300

    atualizando gitignore

--- test.js
+++ test.js
@@ -47,27 +47,3 @@
 //     }
 //   });
 // };
-
-// const fs = require('fs');
-// const arquivosMd = require('./arquivoMD');
-// const lerArquivo = require('./lerArquivo');
-
-// const lerPasta = folder => {
-//   return new Promise((resolve, reject) => {
-//     fs.readdir(folder, 'utf-8', (err, files) => {
-//       files.filter(arquivosMd).reduce((file) => {
-//         let listaLinks = [];
-//         lerArquivo(`${folder}/${file}`).then(mdResult => {
-//           if (err) {
-//             err = 'Link não encontrado';
-//             reject(err)
-//           } else {
-//             resolve(mdResult)
-//           }
-//         });
-//         return Promise.all(listaLinks)
-//       })
-//     });
-//   });
-// };
-// module.exports = lerPasta;
